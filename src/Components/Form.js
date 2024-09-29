b664b748bc7aa79f7beb684f3eb18e93554e2eed
Mr_MoheGit <moheone12@gmail.com>
2024-09-29 17:57:16 +0300

    contact me perfectly functional

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,18 +2,39 @@ import React from 'react'
 import './Form.css'
 
 const Form = () => {
+  
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "fd627a8a-d379-4839-a09f-aea6626f5b79");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message)
+    }
+  };
   return (
     <div className='form'>
-        <form>
-            <lable>Your Name</lable>
-            <input type='tex'></input>
-            <lable>Email</lable>
-            <input type='text'></input>
-            <lable>Subject</lable>
-            <input type='text'></input>
-            <lable>Message</lable>
-            <textarea rows='6' placeholder='Type your Message here'/>
-            <button className='btn'>Submit</button>
+        <form onSubmit={onSubmit}>
+            <label>Your Name</label>
+            <input type='text' placeholder='enter your name' name='name'></input>
+            <label>Email</label>
+            <input type='email' placeholder='enter your email' name='email'></input>
+            <label>Message</label>
+            <textarea rows='6' placeholder='Type your Message here' name='message'/>
+            <button className='btn' type='submit'>Submit Now</button>
         </form>
     </div>
   )
--- src/Components/WorkCard.js
+++ src/Components/WorkCard.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 const WorkCard = (props) => {
   return (
-    <div className="project-card">
+    <div className="project-card block" >
     <img src={props.imagesrc} alt="" />
     <h2 className="project-title">{props.title}</h2>
     <div className="pro-details">
